48392349b1e254d8afe53687eba4a4fd06f81b9e
mmounithar-eng <mmounithar@gmail.com>
2025-10-29 13:59:49 +0530

    Add files via upload

---
+++ mouni 23.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int num1, num2;
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    if (num1 == num2) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
+
+    return 0;
+}
